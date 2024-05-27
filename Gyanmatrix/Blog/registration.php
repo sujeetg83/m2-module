18a8afed006823ed31dab2fc2ad904d2fffe6708
Sujeet Gupta <sujeetggyanmatrix@gmail.com>
2024-05-27 15:24:47 +0530

    Adding Module by composer

---
+++ Gyanmatrix/Blog/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category   Custom
+ * @package    Custom_Manufacturer
+ * @author     Sujeet Gupta
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Gyanmatrix_Blog',
+    __DIR__
+);
